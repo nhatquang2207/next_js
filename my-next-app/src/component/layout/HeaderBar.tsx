736bc365bb21b530262bc58d846f3f3c64bf0ed9
nhatquang2207 <nhatquangkute12@gmail.com>
2024-06-10 17:27:46 +0700

    responsive

--- my-next-app/src/component/layout/HeaderBar.tsx
+++ my-next-app/src/component/layout/HeaderBar.tsx
@@ -1,31 +1,30 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import logo from "../../../public/images/logo.png"
 function HeaderBar() {
   return (
-    <div>
-      <div className="md:w-58 fixed z-10 flex min-w-full flex-row items-center justify-between border-b-2 border-b-yellow-300 bg-[rgba(7,39,48,0.8)] p-4 backdrop-blur sm:w-12 lg:w-full ">
-        <a href="http://localhost:5173/">
+    <div className="snap-start">
+      <div className="fixed z-10 flex min-w-full flex-row items-center justify-between border-b-2 border-b-yellow-300 bg-[rgba(7,39,48,0.8)] p-4 backdrop-blur ">
+        <a href="http://localhost:3000/">
           {" "}
           <Image
-            src="/images/logo.png"
+            src={logo}
             alt="logo"
-            width={120}
-            height={20}
             priority={true}
-            className=" ml-6 flex w-44 items-center justify-center lg:w-full "
+            className=" ml-6 flex items-center justify-center w-44 max-sm:w-28 lg:w-44"
           />
         </a>
         <div>
-          <Link href="/contact-us">
-            <button className="mr-10 flex items-center rounded-3xl bg-blue-600 p-2 ">
+          <Link href="/contact-us"> 
+            <button className="mr-10 flex items-center rounded-3xl bg-blue-600 p-2  ">
               <svg
                 xmlns="ht tp://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="mr-2 h-8 w-8 "
+                className="mr-2 w-8 max-sm:w-6"
               >
                 <path
                   strokeLinecap="round"
@@ -33,7 +32,7 @@ function HeaderBar() {
                   d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
                 />
               </svg>
-              <p>Contact Us</p>
+              <p className="text-12 lg:text-xl">Contact Us</p>
             </button>
           </Link>
         </div>
--- my-next-app/src/component/layout/header.tsx
+++ my-next-app/src/component/layout/header.tsx
@@ -2,88 +2,101 @@ import React from "react";
 import Image from "next/image";
 import section from "/next_js/my-next-app/public/images/bg-section-1.png";
 import qr from "/next_js/my-next-app/public/images/codeQR.png";
-
+import logo from "../../../public/images/logo.png";
 export default function Header() {
   return (
-    <div className="snap-center ">
-      <div className="relative">
-        <Image src={section} alt="" className="h-[80%] lg:w-full " />
-        <div className="absolute left-24 top-24 ">
-          <div className=" font-serif text-[50px] font-semibold text-yellow-200 md:text-12">
-            <p>Grow Through</p>
-            <p>Connectivity!</p>
+    <div className=" ">
+      <div className="relative flex items-center justify-center   ">
+        <Image
+          src={section}
+          alt=""
+          className=" w-screen max-sm:h-screen max-sm:object-cover  "
+          placeholder="blur"
+          sizes="100vw"
+        />
+        <div className="">
+          <div className=" absolute left-0 top-[16%] w-screen py-10 text-center text-lg lg:left-[10%] ">
+            <div className=" font-serif font-semibold text-yellow-200 lg:text-start  ">
+              <p className="w-full lg:text-4xl">
+                Grow Through <br /> Connectivity!
+              </p>
+            </div>
+            <div className="">
+              <p className="pb-10 pt-4  text-white lg:text-start lg:text-3xl ">
+                Connect with your local peer and <br /> business partners in the
+                food industry
+              </p>
+              <div className="flex justify-center lg:justify-start">
+                <Image src={qr} alt="" className="flex w-36  " />
+              </div>
+              <p className=" pt-6  text-yellow-200 lg:text-start lg:text-3xl">
+                {" "}
+                Download now
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="pb-10 pt-4 text-2xl text-white">
-              Connect with your local peers and <br /> business partners in the
-              food industry
-            </p>
-
-            <Image src={qr} alt="" className="w-36" />
-            <p className=" pt-6 text-xl text-yellow-200"> Download now</p>
-          </div>
+          <div></div>
         </div>
       </div>
-      <div className="bg-blue-300 py-4  ">
-        <p className="text-center text-2xl font-bold text-blue-800 ">
+
+      <div className="snap-center bg-blue-300  p-1  ">
+        <p className="text-center text-sm font-bold text-blue-800 lg:text-3xl ">
           Grow Through Connectivity! Connect with your local peers and business
           partners in the food industry Scan to download Download Now From Farm
           to Fork, Where Your Local Food Community Connects
         </p>
-        <div className=" flex w-full snap-y  space-x-4 overflow-x-hidden whitespace-nowrap pt-4 text-xl font-bold text-blue-600    ">
-          <div className=" ml-4 flex animate-marquee items-center space-x-4  ">
-            <p className="   rounded-full bg-white p-4  py-2 text-center ">
+        <div className=" flex w-full snap-y  space-x-4  whitespace-nowrap pt-1  font-bold text-blue-600    ">
+          <div className=" ml-10 flex animate-marquee items-center space-x-4 text-sm lg:text-xl ">
+            <p className="   rounded-full bg-white p-1 text-center lg:p-4 ">
               Restaurant Owner
             </p>
-            <p className=" mx-4  rounded-full bg-[#CCCC33] p-4  py-2  text-center ">
+            <p className="   rounded-full bg-[#CCCC33] p-1 text-center lg:p-4 ">
               Digital Marketing
             </p>
-            <p className=" rounded-full  bg-blue-800 p-4 py-2  text-center">
+            <p className=" rounded-full  bg-blue-800 p-1 text-center lg:p-4">
               Regulation Expert
             </p>
-            <p className=" rounded-full  bg-blue-100 p-4 py-2 text-center ">
+            <p className=" rounded-full  bg-blue-100 p-1 text-center lg:p-4 ">
               Tax Advisor
             </p>
-            <p className=" rounded-full bg-white p-4  py-2 ">Bartender</p>
-            <p className=" mx-4  rounded-full  bg-blue-400 p-4 py-2 ">
-              Cafe Owner
-            </p>
-            <p className=" rounded-full   bg-[#CCCC33] p-4  py-2 ">Farmer</p>
-            <p className=" rounded-full    bg-blue-800 p-4 py-2 ">
+            <p className=" rounded-full bg-white p-1 lg:p-4 ">Bartender</p>
+            <p className=" rounded-full bg-blue-400 p-1  lg:p-4">Cafe Owner</p>
+            <p className=" rounded-full   bg-[#CCCC33] p-1 lg:p-4 ">Farmer</p>
+            <p className=" rounded-full   bg-blue-800 p-1  lg:p-4">
               Marketing Expert
             </p>
-            <p className=" rounded-full    bg-blue-100 p-4  py-2 ">
+            <p className=" rounded-full  bg-blue-100 p-1 lg:p-4 ">
               Maintenance Service
             </p>
-            <p className=" rounded-full   bg-blue-400 p-4 py-2 ">
+            <p className=" rounded-full   bg-blue-400 p-1 lg:p-4  ">
               Grocery Supplier
             </p>
           </div>
-          <div className=" ml-4 flex animate-marquee items-center space-x-4   ">
-            <p className="   rounded-full bg-white p-4  py-2 text-center ">
+          <div className=" ml-10 flex animate-marquee items-center space-x-4  lg:p-4 ">
+            <p className=" rounded-full bg-white p-1 text-center lg:p-4">
               Restaurant Owner
             </p>
-            <p className=" mx-4  rounded-full bg-[#CCCC33] p-4  py-2  text-center ">
+            <p className="  rounded-full bg-[#CCCC33] p-1 text-center  lg:p-4 ">
               Digital Marketing
             </p>
-            <p className=" rounded-full  bg-blue-800 p-4 py-2  text-center">
+            <p className=" rounded-full  bg-blue-800 p-1 text-center  lg:p-4">
               Regulation Expert
             </p>
-            <p className=" rounded-full  bg-blue-100 p-4 py-2 text-center ">
+            <p className=" rounded-full  bg-blue-100 p-1 text-center lg:p-4 ">
               Tax Advisor
             </p>
-            <p className=" rounded-full bg-white p-4  py-2 ">Bartender</p>
-            <p className=" mx-4  rounded-full  bg-blue-400 p-4 py-2 ">
+            <p className=" rounded-full bg-white p-1 lg:p-4 ">Bartender</p>
+            <p className=" mx-4  rounded-full  bg-blue-400 p-1 lg:p-4 ">
               Cafe Owner
             </p>
-            <p className=" rounded-full   bg-[#CCCC33] p-4  py-2 ">Farmer</p>
-            <p className=" rounded-full    bg-blue-800 p-4 py-2 ">
+            <p className=" rounded-full bg-[#CCCC33] p-1 lg:p-4 ">Farmer</p>
+            <p className=" rounded-full bg-blue-800 p-1 lg:p-4 ">
               Marketing Expert
             </p>
-            <p className=" rounded-full    bg-blue-100 p-4  py-2 ">
+            <p className=" rounded-full bg-blue-100 p-1 lg:p-4 ">
               Maintenance Service
             </p>
-            <p className=" rounded-full   bg-blue-400 p-4 py-2 ">
+            <p className=" rounded-full bg-blue-400 p-1 lg:p-4 ">
               Grocery Supplier
             </p>
           </div>
